01274b08b3e307dc0f4fb05e21f1a32fd2973fa7
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-14 17:01:11 +0100

    first challenge logic completed

--- advent_day10.rb
+++ advent_day10.rb
@@ -12,44 +12,49 @@ data = "[({(<(())[]>[[{[]{<()<>>
 arr = []
 data.each_line{ |line| arr << line.gsub("\n", "").split("")}
 
-
-hash = {
-  square: 0,
-  parenthesis: 0,
-  curly: 0,
-  lessthan: 0,
-}
-
-example = "{([(<{}[<>[]}>{[]{[(<()>".split("")
-count = 0
-corrupt = false
-while count < example.length && corrupt = false
-  if char == "]"
-    count2 = index
-    char_count = -1
-    while count2 > 0 do
-      if example[count2] == "["
-        char_count += 1
-        if char_count == 0
-          count2 = 0
-        end
-      elsif example[count2] == "]"
-        char_count -= 1
-      end
-      count2 -= 1
-    end
+def reverse_symbol(symbol)
+  case symbol
+  when "]"
+    return "["
+  when ")"
+    return "("
+  when "}"
+    return "{"
+  when ">"
+    return "<"
   end
-  count += 1
 end
 
+hash = {first: 0, second: 0, third: 0, fourth: 0}
 
-
-
-# example.each_with_index do |char, index|
-#   if char == "]" || char == ")" || char == "}" || char == ">"
-#     count = index
-#     while count > 0 do
-#       # Go back to where this char closes and check if there are any unclosed things inside
-#     end
-#   end
-# end
+arr.each do |line|
+  opening_tags = []
+  count = 0
+  corrupt = nil
+  while count < line.length do
+    if line[count] == "[" || line[count] == "(" || line[count] == "{" || line[count] == "<"
+      opening_tags << line[count]
+    elsif line[count] == "]" || line[count] == ")" || line[count] == "}" || line[count] == ">"
+      if opening_tags.last == reverse_symbol(line[count])
+        opening_tags.pop
+      else
+        corrupt = line[count]
+        count = 10000
+      end
+    end
+  count += 1
+  end
+  if corrupt
+    case corrupt
+    when "]"
+      hash[:first] += 1
+    when ")"
+      hash[:second] += 1
+    when "}"
+      hash[:third] += 1
+    when ">"
+      hash[:fourth] += 1
+    end
+  end
+end
+p hash[:first]*57 + hash[:second]*3 + hash[:third]*1197 + hash[:fourth]*25137
