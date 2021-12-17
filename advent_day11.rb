88d4de4e62ae309b85faa614cab132365d9c4ed9
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-17 13:14:21 +0100

    day11 part1 done

--- advent_day11.rb
+++ advent_day11.rb
@@ -1,9 +1,19 @@
-data = ""
+data = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"
 
 arr = []
 data.each_line{ |line| arr << line.gsub("\n", "").split("").map {|string| string.to_i}}
 
 def recursive_flash_method(args)
+
   to_flash = []
   # For each point check if adjacent exists, then make value += 1. If this results in a value of 10 add to to_flash
   # Then for each point in to_flash increase flash_sum and run recursive_flash_method on that point
@@ -66,8 +76,7 @@ def recursive_flash_method(args)
       to_flash << [args[:point][0]+1, args[:point][1]+1]
     end
   end
-  p "toflash"
-  p to_flash
+
   args[:flash_sum] += to_flash.length
   to_flash.each do |point|
     values = recursive_flash_method({ point: point, arr: args[:arr], flash_sum: args[:flash_sum] })
@@ -78,38 +87,45 @@ def recursive_flash_method(args)
 end
 
 
-test_arr = [[10,8,8],[7,10,8]]
 
-point = [0,0]
-test_sum = 2
-p recursive_flash_method({ point: point, arr: test_arr, flash_sum: test_sum })
-test_arr = recursive_flash_method({ point: point, arr: test_arr, flash_sum: test_sum })[1]
-test_sum = recursive_flash_method({ point: point, arr: test_arr, flash_sum: test_sum })[0]
-point2 = [1,1]
-p recursive_flash_method({ point: point2, arr: test_arr, flash_sum: test_sum })
+# Testing with Day 5 flashes - works!!
+# test_arr = [[10,8,8],[7,10,8]]
+
+# point = [0,0]
+# test_sum = 2
+# values = recursive_flash_method({ point: point, arr: test_arr, flash_sum: test_sum })
+# test_arr = values[1]
+# test_sum = values[0]
+# p test_arr
+# point = [1,1]
+# p recursive_flash_method({ point: point, arr: test_arr, flash_sum: test_sum })
 
 # code below not working must test one point at a time
-# flash_total = 0
-# 10.times do
-#   initial_flashes = []
-#   arr.each_with_index do |line, index|
-#     line.each_with_index do |number, index2|
-#       arr[index][index2] += 1
-#       if arr[index][index2] == 10
-#         initial_flashes << [index, index2]
-#       end
-#     end
-#   end
-#   flash_total += initial_flashes.length
-#   initial_flashes.each do |point|
-#     flash_total = recursive_flash_method({ point: point, arr: arr, flash_sum: flash_total })[0]
-#     arr = recursive_flash_method({ point: point, arr: arr, flash_sum: flash_total })[1]
-#     arr.each_with_index do |line, index|
-#       line.each_with_index do |number, index2|
-#         if arr[index][index2] > 9
-#           arr[index][index2] = 0
-#         end
-#       end
-#     end
-#   end
-# end
+
+flash_total = 0
+100.times do
+  initial_flashes = []
+  arr.each_with_index do |line, index|
+    line.each_with_index do |number, index2|
+      arr[index][index2] += 1
+      if arr[index][index2] == 10
+        initial_flashes << [index, index2]
+      end
+    end
+  end
+
+  flash_total += initial_flashes.length
+  initial_flashes.each do |point|
+    values = recursive_flash_method({ point: point, arr: arr, flash_sum: flash_total })
+    flash_total = values[0]
+    arr = values[1]
+  end
+  arr.each_with_index do |line, index|
+    line.each_with_index do |number, index2|
+      if arr[index][index2] > 9
+        arr[index][index2] = 0
+      end
+    end
+  end
+end
+p flash_total
