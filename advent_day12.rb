97e6e8d124ac1892b05eacf754777847905ae664
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-18 01:03:11 +0100

    Recursive path search function building stages -not working yet

--- advent_day12.rb
+++ advent_day12.rb
@@ -15,7 +15,7 @@ arr.each do |link|
   unless data_hash.key?(link[0])
     data_hash[link[0]] = []
   end
-  # If hash doesn't have the right side as key
+  # If hash doesn't have the right side as key without adding end
   if data_hash.key?(link[1]) == false && link[1] != "end"
     data_hash[link[1]] = []
   end
@@ -23,9 +23,30 @@ arr.each do |link|
   unless link[0] == "start" || link[1] == "end"
     data_hash[link[1]] << link[0]
   end
-
   data_hash[link[0]] << link[1]
-
-
 end
-p data_hash
+
+def recursive_path_search(args)
+
+  p args[:path_arr]
+  p "this is option: #{args[:option]}"
+
+  args[:path_arr] << args[:option]
+
+  if args[:option] != "end" && args[:path_arr].length > 2 && args[:path_arr][(args[:path_arr].length-2)] != args[:option]
+    args[:data_hash][args[:option]].each do |option|
+      recursive_path_search({path_arr: args[:path_arr], option: option, data_hash: args[:data_hash]})
+    end
+  else
+    "Path Done"
+  end
+
+
+  return args[:path_arr]
+end
+p data_hash["start"][0]
+p data_hash["start"][1]
+path_arr = []
+data_hash["start"].each do |option|
+  path_arr = recursive_path_search({path_arr: path_arr, option: option, data_hash: data_hash})
+end
