068c8ac2f9fd0245e6184b301db8e8ca054aa044
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-19 14:20:55 -0500

    small and medium problem works but big out doesn't

--- advent_day12.rb
+++ advent_day12.rb
@@ -1,10 +1,21 @@
-data = "start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end"
+data = "fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"
 
 arr = []
 data.each_line do |line|
@@ -29,20 +40,47 @@ arr.each do |line|
   hash[line[1]] << line[0] unless line[0] == "start" || line[1] == "end"
 end
 
-def path_search(cave, hash)
-  p cave
-  hash[cave].each do |cave_choice|
-    return true if cave_choice == "end"
+class Node
+  attr_accessor :val, :children
 
-    path_search(cave_choice, hash)
-  end
-
-  return false
-end
-
-finished = false
-while finished == false do
-  hash["start"].each do |cave|
-    finished = path_search(cave, hash)
+  def initialize(val)
+    @val = val
+    @children = []
   end
 end
+
+def node_declaration(cave, hash, smallCaves, currentPath)
+  currentPath = currentPath + [cave.val]
+  if currentPath[-1] == "end"
+    p currentPath
+  end
+  if cave.val != cave.val.upcase && cave.val != "end"
+    smallCaves = smallCaves + [cave.val]
+  end
+  # hash key exists?
+  if hash[cave.val]
+    hash[cave.val].each do |option|
+      unless smallCaves.include? option
+        temp = Node.new(option)
+        cave.children << temp
+        node_declaration(temp, hash, smallCaves, currentPath)
+      end
+    end
+  end
+end
+
+def dfs(node,count)
+  count += 1 if node.val == "end"
+
+  node.children.each do |child|
+    count = dfs(child, count)
+  end
+  count
+end
+
+root = Node.new("start")
+smallCaves = []
+currentPath = []
+node_declaration(root, hash, smallCaves, currentPath)
+count = 0
+print dfs(root, count)
--- test.rb
+++ test.rb
@@ -1,33 +1,28 @@
-start = [3,4,3,1,2]
+class Node
+  attr_accessor :val, :children
 
-def hash(start)
-  hash = {8 => 0, 7 => 0, 6 => 0, 5 => 0, 4 => 0, 3 => 0, 2 => 0, 1 => 0, 0 => 0}
-  start.each do |number|
-    hash[number] += 1
+  def initialize(val)
+    @val = val
+    @children = []
   end
-  return hash
 end
+root = Node.new(3)
+child_l = Node.new(9)
+child_r = Node.new(20)
+grand_child_r_l = Node.new(15)
+grand_child_r_r = Node.new(7)
+grand_child_l_l = Node.new(33)
+child_r.children << grand_child_r_l
+child_r.children << grand_child_r_r
+child_l.children << grand_child_l_l
+root.children << child_r
+root.children << child_l
 
-hash = hash(start)
+def dfs(node)
+  p node.val
 
-def reproduce(days, hash)
-  count = 0
-  while count < days
-    hash2 = {8 => 0, 7 => 0, 6 => 0, 5 => 0, 4 => 0, 3 => 0, 2 => 0, 1 => 0, 0 => 0}
-    hash2[8] = hash[0]
-    hash2[7] = hash[8]
-    hash2[6] = hash[7] + hash[0]
-    hash2[5] = hash[6]
-    hash2[4] = hash[5]
-    hash2[3] = hash[4]
-    hash2[2] = hash[3]
-    hash2[1] = hash[2]
-    hash2[0] = hash[1]
-    hash = hash2
-    p hash
-    count += 1
+  node.children.each do |child|
+    dfs(child)
   end
-  return hash
 end
-
-p reproduce(18, hash)
+dfs(root)
